133b021daa862ee52396ff541a75d4056ab9d025
Brian Hague <bhague1281@gmail.com>
2015-04-24 19:07:21 -0400

    Add favorite button with title/id data

--- ruby-backend-starter-code/public/js/app.js
+++ ruby-backend-starter-code/public/js/app.js
@@ -8,9 +8,19 @@
     xhr.addEventListener('load', function(response){
       var res =  JSON.parse(this.response).Search;
       for(var i = 0; i < res.length; i++){
+        // create movie title list item
         var node = document.createElement('li');
         node.innerText = res[i].Title;
+
+        // create favorite button
+        var favoriteButton = document.createElement('button');
+        favoriteButton.setAttribute('data-oid', res[i].imdbID);
+        favoriteButton.setAttribute('data-name', res[i].Title);
+        favoriteButton.innerText = 'Favorite';
+
+        // append movie title and favorite button to list
         document.querySelector('ul').appendChild(node);
+        document.querySelector('ul').appendChild(favoriteButton);
       }
   });
     xhr.send();
